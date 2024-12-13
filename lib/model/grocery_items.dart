0ff688d6323e1812c8d9af2cc2bd78ad4601f92b
Mohamed Raashid <raashidm862@gmail.com>
2024-12-13 16:11:40 +0530

    create jsom data and model class

---
+++ lib/model/grocery_items.dart
@@ -0,0 +1,25 @@
+class GroceryItem {
+  final int id;
+  final String category;
+  final String name;
+  final double price;
+  final String image;
+
+  GroceryItem({
+    required this.id,
+    required this.category,
+    required this.name,
+    required this.price,
+    required this.image,
+  });
+
+  factory GroceryItem.fromJson(Map<String, dynamic> json) {
+    return GroceryItem(
+      id: json['id'],
+      category: json['category'],
+      name: json['name'],
+      price: json['price'].toDouble(),
+      image: json['image'],
+    );
+  }
+}
--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +7,34 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Main Screen')),
+      appBar: AppBar(
+        title: Text(
+          'Fresh Mart',
+          style: GoogleFonts.anton(color: Colors.white, fontSize: 28),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: Container(
+              height: 40,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white.withOpacity(0.8),
+              ),
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.shopping_cart,
+                  color: Colors.green,
+                  size: 20,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
       body: const Center(
         child: Text('Welcome to the Main Screen!'),
       ),
